30cfb7e1fd8468decdaaf20c89582ce5ef9d1c6c
IljaFonarevs <125666110+IljaFonarevs@users.noreply.github.com>
2023-03-19 21:49:09 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,37 +1,33 @@
+import math
+
 def build_heap(data):
     swaps = []
-    for j in range ((len(data)-2)//2, -1, -1):
-        siftDown(data, j, len(data),swaps)
-    for end in range(len(data)-1, 0, -1):
-        swap(data, 0, end, swaps)
-        siftDown(data, 0, end, swaps) 
+    for j in range (math.floor(len(data)/2), -1, -1):
+        siftDown(data, j,swaps)
+    for end in range(len(data), 0, -1):
+        # swap(data, 0, end, swaps)
+        siftDown(data, end, swaps) 
     return swaps
 
 def swap(data, i, j, swaps):
     data[i], data[j] = data[j], data[i]
-    swaps.append([i, j])
-def siftDown(data, i, length,swaps):
+    swaps.append([j, i])
+    return swaps
+def siftDown(data, i,swaps):
     l, r = i*2+1, i*2+2
-    while (True):
-        if max(l, r) < length:
-            if data[i] >= min(data[l],data[r]): break
-            elif data[l] > data[r]:
-                swap(data, i, l,swaps)
-                i = l
-            else:
-                swap(data, i, r,swaps)
-                i = r
-        elif l < length:
-            if data[l] > data[i]:
-                swap(data, i, l, swaps)
-                i = l
-            else: break
-        elif r < length:
-            if data[r] > data[i]:
-                swap(data, i, r, swaps)
-                i = r
-            else: break
-        else: break
+    smallest_num = i
+    if(l < len(data) and data[l] < data[smallest_num]):
+        smallest_num = l
+    if(r < len(data) and data[r] < data[smallest_num]):
+        smallest_num = r
+    if(smallest_num != i):
+        swap(data, smallest_num, i, swaps)
+        siftDown(data, smallest_num, swaps)
+    return swaps
+
+
+
+    
 
 
 def main():
@@ -60,6 +56,7 @@ def main():
                         if len(data) != n:
                             raise ValueError(f"Invalid input, expected {n} elements")
                         swaps = build_heap(data)
+                        print(data)
                         print(len(swaps))
                         for i, j in swaps:
                             print(i, j)
